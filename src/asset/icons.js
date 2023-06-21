240d81b033131772b7a68fb67c05039e61295b3f
Prince <singhprince8222692@gmail.com>
2023-06-22 02:17:39 +0530

    import icons

---
+++ src/asset/icons.js
@@ -0,0 +1,26 @@
+import {
+  AiOutlineHome,
+  AiOutlineHeart,
+  AiFillHeart,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BsBookmark, BsFillBookmarkFill, BsThreeDots } from "react-icons/bs";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { RiRocketLine } from "react-icons/ri";
+import { CgProfile, CgAddR } from "react-icons/cg";
+import { GoComment } from "react-icons/go";
+
+export {
+  AiOutlineHome,
+  AiOutlineHeart,
+  AiFillHeart,
+  BsBookmark,
+  RiRocketLine,
+  BsFillBookmarkFill,
+  IoIosNotificationsOutline,
+  CgProfile,
+  GoComment,
+  CgAddR,
+  BsThreeDots,
+  AiOutlineSearch,
+};
