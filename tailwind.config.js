443528de90bf189e161976273f4a52e86ea4ee7d
Prince <singhprince8222692@gmail.com>
2023-06-22 02:15:03 +0530

    add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: "#4da5e8",
+        orange: "#ff7816",
+        isactiveColor: "#dfe3ed",
+      },
+      height: {
+        128: "42.3rem",
+      },
+      width: {
+        700: "700px",
+      },
+    },
   },
   plugins: [],
 };
