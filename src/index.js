91e225c996da306edbda8b8922310a90e08032bf
Prince <singhprince8222692@gmail.com>
2023-06-22 02:15:41 +0530

    set up

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ToastContainer />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
