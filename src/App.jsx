2c30fd768f147e7814cf0a465e82ecae56f3defa
Prince <singhprince8222692@gmail.com>
2023-06-22 02:14:12 +0530

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./Layout/Layout";
+import {
+  Bookmarks,
+  Explore,
+  Home,
+  LikedPost,
+  Profile,
+  LandingPage,
+  NotFound,
+} from "./pages/index";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/likedPost" element={<LikedPost />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        <Route path="/landingPage" element={<LandingPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
