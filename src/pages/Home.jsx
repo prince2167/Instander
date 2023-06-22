b6a9f8b1132f768ecce67a1abda233699f95d7ae
Prince <singhprince8222692@gmail.com>
2023-06-22 23:27:35 +0530

    render post list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { ShimmerPostCard } from "../ShimmerCard/ShimmerPostCard";
+import { CreatePsot } from "../components/index";
 
 const Home = () => {
   return (
     <div className="w-700">
+      <CreatePsot />
       <ShimmerPostCard />
     </div>
   );
