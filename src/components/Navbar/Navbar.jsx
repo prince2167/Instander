681876920b2a4efb21c5425286e694cd97c43f4c
Prince <singhprince8222692@gmail.com>
2023-06-23 23:47:43 +0530

    create navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
       <NavLink to="/" className="flex items-center gap-4">
         <img
           className="h-16 w-16"
-          src="https://cdn-icons-png.flaticon.com/512/1005/1005491.png?w=740&t=st=1687350364~exp=1687350964~hmac=8d1a0522c2ff4e92133d00db21fabe68972ccc8d2d99b660caea9a669e988e66"
+          src="https://res.cloudinary.com/drre76xpz/image/upload/v1687520238/1005491_qrzn8x.png"
           alt=""
         />
         <h1 className="font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-lightBlue to-orange">
