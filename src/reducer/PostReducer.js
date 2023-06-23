63f694630e33e27e227d7de06c00c88d24bd54c1
Prince <singhprince8222692@gmail.com>
2023-06-23 23:42:31 +0530

    add reducer

---
+++ src/reducer/PostReducer.js
@@ -0,0 +1,13 @@
+export const initialState = {};
+
+export const postReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
