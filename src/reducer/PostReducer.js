997e943cf03d6d158dc75931663ad9c68a49216c
Prince <singhprince8222692@gmail.com>
2023-07-04 21:05:55 +0530

    create post reducer

--- src/reducer/PostReducer.js
+++ src/reducer/PostReducer.js
@@ -1,13 +1,19 @@
-export const initialState = {};
+export const initialState = {
+  posts: [],
+};
 
 export const postReducer = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case value:
-      break;
-
-    default:
-      break;
+    case "GET_ALL_POSTS": {
+      return {
+        ...state,
+        posts: payload,
+      };
+    }
+    default: {
+      return state;
+    }
   }
 };
