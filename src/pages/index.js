3ad37e134e688a779695dd03bf98797be474b3d0
Prince <singhprince8222692@gmail.com>
2023-07-04 21:05:26 +0530

    feat: signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import Home from "./Home";
 import Explore from "./Explore";
 import Bookmarks from "./Bookmarks";
-import LandingPage from "./LandingPage";
 import LikedPost from "./LikedPost";
 import Profile from "./Profile";
 import NotFound from "./NotFound";
+import Login from "./Login";
+import Signup from "./Signup";
 export {
   Home,
   Explore,
   Bookmarks,
-  LandingPage,
   LikedPost,
   Profile,
   NotFound,
+  Login,
+  Signup,
 };
