c798b0cb386f77f59d86deeb9e4d420f1815f87a
Prince <singhprince8222692@gmail.com>
2023-07-04 21:03:33 +0530

    add context provider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { PostProvider } from "./contexts/post-context";
+import { AuthProvider } from "./contexts/auth-context";
+import { UserProvider } from "./contexts/user-context";
+import { Toaster } from "react-hot-toast";
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ToastContainer />
-      <App />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: "1rem",
+          right: "1rem",
+          fontSize: "0.9rem",
+        }}
+      />
+      <AuthProvider>
+        <UserProvider>
+          <PostProvider>
+            <App />
+          </PostProvider>
+        </UserProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
