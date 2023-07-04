fae6fc64fe52f3954f404be13c6e4a72dd09525d
Prince <singhprince8222692@gmail.com>
2023-07-04 21:02:22 +0530

    import components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,24 @@
 import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
-import Login from "./Authentication/Login";
-import Signup from "./Authentication/Signup";
+import LoginForm from "./Authentication/LoginForm";
+import SignupForm from "./Authentication/SignupForm";
 import Searchbar from "./Suggestionbar/Searchbar";
 import SuggestionAccounts from "./Suggestionbar/SuggestionAccounts";
-import CreatePsot from "./CreatePost/CreatePsot";
-export { Navbar, Sidebar, Login, Signup, Searchbar, SuggestionAccounts,CreatePsot};
+import CreatePost from "./CreatePost/CreatePost";
+import PostList from "./Posts/PostList";
+import PostCard from "./Posts/PostCard";
+import Comments from "./Comments/Comments";
+import CommentItem from "./Comments/CommentItem";
+export {
+  Navbar,
+  Sidebar,
+  LoginForm,
+  SignupForm,
+  Searchbar,
+  SuggestionAccounts,
+  CreatePost,
+  PostList,
+  PostCard,
+  Comments,
+  CommentItem,
+};
