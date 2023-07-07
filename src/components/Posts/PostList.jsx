efe17874d26adf3d5558ee46d193e3327713d5c5
Prince <singhprince8222692@gmail.com>
2023-07-07 21:09:19 +0530

    create posts ui

--- src/components/Posts/PostList.jsx
+++ src/components/Posts/PostList.jsx
@@ -1,9 +1,11 @@
 import { PostCard } from "../index";
 
 const PostList = ({ posts }) => {
-  
   return (
-    <div className="flex flex-wrap gap-4">{posts.length && posts.map((post) => <PostCard post={post} key={post.id} />)}</div>
+    <div className="flex flex-wrap gap-4">
+      {posts.length &&
+        posts.map((post) => <PostCard post={post} key={post.id} />)}
+    </div>
   );
 };
 
