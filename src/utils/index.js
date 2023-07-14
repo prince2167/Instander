b0d7c26cd8772b036b7a751f2f7c075d55b6af24
Prince <singhprince8222692@gmail.com>
2023-07-14 23:33:40 +0530

    feat: sorted method

---
+++ src/utils/index.js
@@ -0,0 +1,16 @@
+export const getSortedPosts = (posts, sortBy) => {
+  if (sortBy === "Trending") {
+    return [...posts].sort((a, b) => b.likes.likeCount - a.likes.likeCount);
+  }
+  if (sortBy === "Latest") {
+    return [...posts].sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+  }
+  if (sortBy === "Oldest") {
+    return [...posts].sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    );
+  }
+  return [...posts];
+};
