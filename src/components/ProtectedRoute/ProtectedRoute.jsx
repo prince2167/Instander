b7bd17bbab9e6009c47af56a9d48dbd697156108
Prince <singhprince8222692@gmail.com>
2023-07-14 23:28:27 +0530

    create protected route

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../contexts/auth-context";
+
+const ProtectedRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
