2373b6f5c894affe07929de7aeb9023434018cdd
Prince <singhprince8222692@gmail.com>
2023-07-14 23:30:09 +0530

    create services for comments

---
+++ src/services/commentServices.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const getComments = (postId) => axios.get(`/api/comments/${postId}`);
+
+const addComment = (postId, commentData,token) =>{
+  
+  axios.post(
+    `/api/comments/add/${postId}`,
+    { commentData },
+    {
+      headers: { authorization: token },
+    }
+  );
+}
+const editComment = (postId, commentId, commentData,token) =>
+  axios.post(
+    `/api/comments/edit/${postId}/${commentId}`,
+    { commentData },
+    {
+      headers: { authorization: token  },
+    }
+  );
+
+const deleteComment = (postId, commentId,token) =>
+  axios.delete(`/api/comments/delete/${postId}/${commentId}`, {
+    headers: {authorization: token  },
+  });
+
+export {getComments, addComment, editComment, deleteComment };
