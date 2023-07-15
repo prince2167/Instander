700bcddcdcbb1a17cb56f67039740e7b556a3847
Prince <singhprince8222692@gmail.com>
2023-07-15 11:21:38 +0530

    render like posts

--- src/pages/LikedPost.jsx
+++ src/pages/LikedPost.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useAuth } from "../contexts/auth-context";
+import { usePosts } from "../contexts/post-context";
+import { ShimmerPostCard } from "../ShimmerCard/ShimmerPostCard";
+import { PostCard } from "../components/index";
 
 const LikedPost = () => {
-  return <div  className="w-700">LikedPost</div>;
+  const { state, isLoading } = usePosts();
+  const { posts } = state;
+  const { currentUser } = useAuth();
+
+  const likedPosts = posts.filter((post) =>
+    post.likes.likedBy.find((user) => user.username === currentUser.username)
+  );
+
+  if (isLoading) return <ShimmerPostCard />;
+  return (
+    <div className="w-700 mt-5">
+      {likedPosts?.length > 0 ? (
+        <div className="flex flex-wrap gap-4">
+          {likedPosts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-orange text-2xl text-center font-semibold">
+          No Liked Posts Yet!
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default LikedPost;
