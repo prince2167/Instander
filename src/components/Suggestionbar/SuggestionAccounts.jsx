573182a9e2ad45a399ec9fc0ff568c8e0ad141f9
Prince <singhprince8222692@gmail.com>
2023-07-15 11:20:00 +0530

    route for account details

--- src/components/Suggestionbar/SuggestionAccounts.jsx
+++ src/components/Suggestionbar/SuggestionAccounts.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import { ShimerUserCard } from "../../ShimmerCard/ShimerUserCard";
 import { useAuth } from "../../contexts/auth-context";
 import { useUser } from "../../contexts/user-context";
 import { Searchbar } from "../index";
 
 const SuggestionAccountsBar = () => {
-  const { userState } = useUser();
+  const { userState, followUserHandler, isLoading } = useUser();
   const { users } = userState;
   const { currentUser } = useAuth();
-
+  const navigate = useNavigate();
   const suggestionAccounts = users
     ?.filter((user) => user.username !== currentUser?.username)
     ?.filter(
@@ -15,28 +17,41 @@ const SuggestionAccountsBar = () => {
           (account) => account.username === user.username
         )
     );
-  console.log(suggestionAccounts);
+
+  if (isLoading) return <ShimerUserCard />;
   return (
     <div className="w-1/3">
       <Searchbar />
       <div className=" mx-10 my-5 flex flex-col gap-4 border border-gray-300 shadow-lg py-2 rounded-md  ">
         <h1 className="text-md ml-2  font-semibold">Who to follow?</h1>
         {suggestionAccounts?.map((user) => (
-          <div key={user._id} className="flex justify-between items-center flex-wrap px-2 py-2 cursor-pointer">
+          <div
+            onClick={() => navigate(`/profile/${user?.username}`)}
+            key={user._id}
+            className="flex justify-between items-center flex-wrap px-2 py-2 cursor-pointer"
+          >
             <div className="flex gap-2 items-center">
               <img
-                src={user.avatarURL}
+                src={user?.avatarURL}
                 alt=""
                 className="h-12 w-12 rounded-full"
               />
               <div>
                 <span className="font-semibold">
-                  {user.firstName} {user.lastName}
+                  {user?.firstName} {user?.lastName}
                 </span>
-                <p>{user.username}</p>
+                <p>{user?.username}</p>
               </div>
             </div>
-            <button className="bg-orange text-white px-4 py-1 rounded-lg cursor-pointer">Follow</button>
+            <button
+              className="bg-orange text-white px-4 py-1 rounded-lg cursor-pointer"
+              onClick={(event) => {
+                event.stopPropagation();
+                followUserHandler(user?._id);
+              }}
+            >
+              Follow
+            </button>
           </div>
         ))}
       </div>
