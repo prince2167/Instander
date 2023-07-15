0e2ea0a6648a715e884f0de7ebf02bb4b0d5cc79
Prince <singhprince8222692@gmail.com>
2023-07-15 11:21:05 +0530

    create profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { usePosts } from "../contexts/post-context";
+import { useUser } from "../contexts/user-context";
+import { PostCard, ProfileDetails } from "../components/index";
+import { useAuth } from "../contexts/auth-context";
 const Profile = () => {
-  return (
-    <div  className="w-700">Profile</div>
-  )
-}
+  const { currentUser } = useAuth();
+  const { fetchUserByUsername, userProfileLoading, userState } = useUser();
+  const { userDetails } = userState;
 
-export default Profile
+  const {
+    state: { posts },
+  } = usePosts();
+
+  const { username } = useParams();
+  const userPosts = posts?.filter((post) => post.username === username);
+  const isEdit = currentUser?.username === username;
+
+  useEffect(() => {
+    fetchUserByUsername(username);
+    window.scroll({ top: 0, behavior: "smooth" });
+  }, [username, currentUser]);
+
+  if (userProfileLoading) return null;
+  return (
+    <div className="w-700 mt-5">
+      <>
+        {userDetails && (
+          <ProfileDetails
+            user={userDetails}
+            totalPost={userPosts.length}
+            isEdit={isEdit}
+          />
+        )}
+
+        <h1 className="text-2xl font-semibold my-4">Posts</h1>
+        {userPosts?.length > 0 ? (
+          <div className="flex flex-wrap gap-4">
+            {userPosts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
+          </div>
+        ) : (
+          <p className=" text-2xl font-semibold text-center">No post yet!</p>
+        )}
+      </>
+    </div>
+  );
+};
+
+export default Profile;
