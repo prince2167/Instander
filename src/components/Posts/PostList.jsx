377fd57c5725aeaac0e18a96abb0b6e688672ce4
Prince <singhprince8222692@gmail.com>
2023-07-15 11:14:50 +0530

    feat: filter for posts

--- src/components/Posts/PostList.jsx
+++ src/components/Posts/PostList.jsx
@@ -1,10 +1,14 @@
+import { usePosts } from "../../contexts/post-context";
+import { getSortedPosts } from "../../utils";
 import { PostCard } from "../index";
 
 const PostList = ({ posts }) => {
+  const { sortBy } = usePosts();
+  const sortedPosts = getSortedPosts(posts, sortBy);
   return (
     <div className="flex flex-wrap gap-4">
-      {posts.length &&
-        posts.map((post) => <PostCard post={post} key={post.id} />)}
+      {sortedPosts?.length &&
+        sortedPosts?.map((post) => <PostCard post={post} key={post.id} />)}
     </div>
   );
 };
