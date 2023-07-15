9404b80937c0b200058bc7b8efa5d01ec7f993a6
Prince <singhprince8222692@gmail.com>
2023-07-15 11:15:34 +0530

    render bookmarks posts

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { ShimmerPostCard } from "../ShimmerCard/ShimmerPostCard";
+import { PostCard, PostList } from "../components";
+import { usePosts } from "../contexts/post-context";
+import { useUser } from "../contexts/user-context";
 
 const Bookmarks = () => {
-  return <div  className="w-700">Bookmarks</div>;
+  const { state } = usePosts();
+  const { posts } = state;
+  const { userState, bookmarkIsLoading } = useUser();
+  const { bookmarks } = userState;
+
+  const bookmarksPosts = [...posts]
+    ?.reverse()
+    ?.filter((post) =>
+      bookmarks?.find((bookmark) => bookmark._id === post._id)
+    );
+
+  if (bookmarkIsLoading) return <ShimmerPostCard />;
+  return (
+    <div className="w-700 mt-5">
+      {bookmarksPosts?.length > 0 ? (
+        <div className="flex flex-wrap gap-4">
+          {bookmarksPosts?.map((post) => (
+            <PostCard key={post?._id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-orange text-2xl  text-center font-semibold">
+          Bookmark posts to see them here!
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Bookmarks;
