a199824e6f76757edbf77e8fafd9722b6d0e4662
Prince <singhprince8222692@gmail.com>
2023-07-15 11:13:15 +0530

    filter posts

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { MdOutlineTune } from "../../asset/icons";
+import { useClickOutside } from "../../hooks/useClickOutside";
+import { usePosts } from "../../contexts/post-context";
 function Filter() {
-  const [sortBy, setSortBy] = useState("Latest");
   const [showSortOptions, setShowSortOptions] = useState(false);
+  const { sortBy, setSortBy } = usePosts();
+  const clickRef = useRef();
+  useClickOutside(clickRef, () => {
+    setShowSortOptions(false);
+  });
+
   return (
     <div className="my-6 flex justify-between items-center px-2">
-      <h1 className="text-xl font-semibold">Latest Post</h1>
-      <div>
+      <h1 className="text-xl font-semibold">{sortBy}</h1>
+      <div ref={clickRef}>
         <button className="cursor-pointer relative">
           <MdOutlineTune
             size={28}
@@ -18,6 +25,7 @@ function Filter() {
                 className={`options hover:bg-isactiveColor hover:text-orange  rounded-lg  px-6 py-1 flex justify-center items-center ${
                   sortBy === "Trending" ? "bg-isactiveColor text-orange " : ""
                 }`}
+                onClick={() => setSortBy("Trending")}
               >
                 Trending
               </li>
@@ -25,6 +33,7 @@ function Filter() {
                 className={`options hover:bg-isactiveColor hover:text-orange  rounded-lg  px-6 py-1 flex justify-center items-center ${
                   sortBy === "Latest" ? "bg-isactiveColor text-orange" : ""
                 }`}
+                onClick={() => setSortBy("Latest")}
               >
                 Latest
               </li>
@@ -32,6 +41,7 @@ function Filter() {
                 className={`options hover:bg-isactiveColor hover:text-orange rounded-lg  px-6 py-1 flex justify-center items-center ${
                   sortBy === "Oldest" ? "bg-isactiveColor text-orange" : ""
                 }`}
+                onClick={() => setSortBy("Oldest")}
               >
                 Oldest
               </li>
