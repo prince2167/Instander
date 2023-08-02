4daa6f411b63d1badbb294bdeda709ddbaf124bf
Prince <singhprince8222692@gmail.com>
2023-08-02 14:36:11 +0530

    feat: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,8 @@ import { LoginForm } from "../components/index";
 
 const Login = () => {
   return (
-    <div className="flex justify-around items-center">
-      <img src={mainImage} alt="" className="w-[50rem]" />
+    <div className="flex  justify-around items-center min-h-screen">
+      <img src={mainImage} alt="" className=" hidden xl:block w-[50rem]" />
       <LoginForm />
     </div>
   );
