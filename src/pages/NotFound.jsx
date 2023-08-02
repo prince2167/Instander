8f8653b704b372921366bf9587a4db7853046c3b
Prince <singhprince8222692@gmail.com>
2023-08-02 15:07:07 +0530

    page not found error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 const NotFound = () => {
   return (
     <>
@@ -13,15 +13,15 @@ const NotFound = () => {
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            </Link>
+            <Link href="/" className="text-sm font-semibold text-gray-900">
               Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </div>
       </main>
