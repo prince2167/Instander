a69e1b1a846c2cc05d7c40d58b7c1654e8af3b95
Prince <singhprince8222692@gmail.com>
2023-08-02 14:34:07 +0530

    style

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -17,7 +17,7 @@ const Explore = () => {
 
   if (isLoading) return <ShimmerPostCard />;
   return (
-    <div className="w-700 mt-5">
+    <div className="xl:w-700  w-full px-4 py-2 relative mb-8 mt-5">
       {explorePost?.length > 0 ? (
         <div className="flex flex-wrap gap-4">
           {explorePost?.map((post) => (
