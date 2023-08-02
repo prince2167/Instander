8cd7e22f34767d5d43c2c59280d5882a6e0b6ff1
Prince <singhprince8222692@gmail.com>
2023-08-02 14:20:19 +0530

    create auth controller

--- src/backend/controllers/AuthController.js
+++ src/backend/controllers/AuthController.js
@@ -44,7 +44,7 @@ export const signupHandler = function (schema, request) {
     const createdUser = schema.users.create(newUser);
     const encodedToken = sign(
       { _id, username },
-      import.meta.env.VITE_APP_JWT_SECRET
+      process.env.REACT_APP_JWT_SECRET
     );
     return new Response(201, {}, { createdUser, encodedToken });
   } catch (error) {
