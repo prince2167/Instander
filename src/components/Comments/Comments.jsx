427b51318c80392955180f93cb96ab88107878e8
Prince <singhprince8222692@gmail.com>
2023-08-02 14:23:37 +0530

    feat: add comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,11 +1,49 @@
-import React from "react";
-import { useAuth } from "../../contexts/auth-context";
+import React, { useState } from "react";
 import { MdSend } from "../../asset/icons";
 import { CommentItem } from "../index";
+import { useAuth } from "../../contexts/auth-context";
+import { usePosts } from "../../contexts/post-context";
 
 const Comments = ({ post }) => {
   const { currentUser } = useAuth();
-  // console.log(post.comments)
+  const { addPostCommentHandler, editUserComment, deleteUserComment } =
+    usePosts();
+  const [commentInput, setCommentInput] = useState("");
+  const [editingComment, setEditingComment] = useState(null);
+
+  const submitCommentHandler = (event) => {
+    event.preventDefault();
+    if (commentInput.length > 0) {
+      addPostCommentHandler({
+        postId: post?._id,
+        commentData: { text: commentInput },
+      });
+      setCommentInput("");
+    }
+  };
+  const handleEditComment = (comment) => {
+    setEditingComment(comment);
+    setCommentInput(comment.text);
+  };
+
+  const commentUpdateHandler = () => {
+    editUserComment({
+      postId: post?._id,
+      commentId: editingComment._id,
+      commentData: {
+        text: editingComment.text,
+      },
+    });
+    setEditingComment(null);
+    setCommentInput("");
+  };
+  const deleteUserCommentHandler = (comment) => {
+    deleteUserComment({
+      postId: post?._id,
+      commentId: comment?._id,
+    });
+  };
+
   return (
     <div className="">
       <div className="flex items-center my-4">
@@ -21,25 +59,49 @@ const Comments = ({ post }) => {
             alt=""
           />
         )}
-        <div className=" w-full relative">
+        <form className=" w-full relative" onSubmit={submitCommentHandler}>
           <input
+            autoFocus
             placeholder="Add a new comment"
             className=" w-full border  ml-2 rounded-md p-2 border-black
             outline-orange"
+            value={editingComment ? editingComment.text : commentInput}
+            onChange={(event) =>
+              editingComment
+                ? setEditingComment({
+                    ...editingComment,
+                    text: event.target.value,
+                  })
+                : setCommentInput(event.target.value)
+            }
           />
 
-          <button className="absolute right-0 top-3">
-            <MdSend size={20} />
-          </button>
-        </div>
+          {editingComment ? (
+            <button
+              className="absolute right-0 top-2 text-orange font-semibold"
+              onClick={commentUpdateHandler}
+            >
+              Update
+            </button>
+          ) : (
+            <button className="absolute right-0 top-3" type="submit">
+              <MdSend size={20} />
+            </button>
+          )}
+        </form>
       </div>
       <div className="flex flex-col gap-2">
         {post?.comments.length > 0 ? (
           post?.comments.map((comment) => (
-            <CommentItem comment={comment} key={comment._id} />
+            <CommentItem
+              comment={comment}
+              key={comment._id}
+              handleEditComment={handleEditComment}
+              deleteUserCommentHandler={deleteUserCommentHandler}
+            />
           ))
         ) : (
-          <span>No comments yet</span>
+          <span className="text-center">No comments yet</span>
         )}
       </div>
     </div>
