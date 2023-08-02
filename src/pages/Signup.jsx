48af8b73bc9abbed5ed8f2d17f12ae61bcfa1e5d
Prince <singhprince8222692@gmail.com>
2023-08-02 14:37:25 +0530

    feat: signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,8 +3,8 @@ import mainImage from "../asset/Image/main.png";
 import { SignupForm } from "../components/index";
 const Signup = () => {
   return (
-    <div className="flex justify-around items-center">
-      <img src={mainImage} alt="" className="w-[50rem]" />
+    <div className="flex  justify-around items-center min-h-screen">
+      <img src={mainImage} alt="" className="hidden xl:block w-[50rem]" />
       <SignupForm />
     </div>
   );
