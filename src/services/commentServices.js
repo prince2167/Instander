95e804bce7d67ee033b65c2942651af32287586e
Prince <singhprince8222692@gmail.com>
2023-08-02 14:40:21 +0530

    create services for comments

--- src/services/commentServices.js
+++ src/services/commentServices.js
@@ -2,28 +2,24 @@ import axios from "axios";
 
 const getComments = (postId) => axios.get(`/api/comments/${postId}`);
 
-const addComment = (postId, commentData,token) =>{
-  
-  axios.post(
+const addComment = async (postId, commentData, token) =>
+  await axios.post(
     `/api/comments/add/${postId}`,
+    { commentData },
+    { headers: { authorization: token } }
+  );
+const editComment = (postId, commentId, commentData, token) =>
+  axios.post(
+    `/api/comments/edit/${postId}/${commentId}`,
     { commentData },
     {
       headers: { authorization: token },
     }
   );
-}
-const editComment = (postId, commentId, commentData,token) =>
-  axios.post(
-    `/api/comments/edit/${postId}/${commentId}`,
-    { commentData },
-    {
-      headers: { authorization: token  },
-    }
-  );
 
-const deleteComment = (postId, commentId,token) =>
+const deleteComment = (postId, commentId, token) =>
   axios.delete(`/api/comments/delete/${postId}/${commentId}`, {
-    headers: {authorization: token  },
+    headers: { authorization: token },
   });
 
-export {getComments, addComment, editComment, deleteComment };
+export { getComments, addComment, editComment, deleteComment };
