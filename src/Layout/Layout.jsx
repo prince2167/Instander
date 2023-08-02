621412f24758eb211738c6f0b50961ef7f54a90a
Prince <singhprince8222692@gmail.com>
2023-08-02 14:18:23 +0530

    style change

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,16 +3,16 @@ import { Navbar, Sidebar, SuggestionAccounts } from "../components/index";
 
 const Layout = () => {
   return (
-    <>
+    <div className=" min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex justify-between">
+      <div className="flex   sm:mb-0">
         <Sidebar />
         <main>
           <Outlet />
         </main>
         <SuggestionAccounts />
       </div>
-    </>
+    </div>
   );
 };
 
